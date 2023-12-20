58dc55a894fa80d83ee5b4b7aedf5db70dea85ac
magnus <magnus.maynard@hotmail.com>
2023-12-20 20:44:42 +0000

    Exposing capture args.

--- capture/ratcam_capture/__init__.py
+++ capture/ratcam_capture/__init__.py
@@ -5,8 +5,12 @@ import os
 
 @click.command(help="Capture a video from RPI an camera and save to disk")
 @click.option("--output-directory", "-o", required=True, type=str, help="Path to output directory where videos are saved")
-def main(output_directory: str) -> bool:
-    if capture(output_dir=output_directory):
+@click.option("--split", "-s", type=int, help="Seconds between each video split",
+    show_default=True, default=600)
+@click.option("--flip", type=int, help="Flip orientation of output video. 0=none, 1=CCW, 2=180, 3=CW", show_default=True, default=2)
+@click.option("--fps", type=int, help="FPS of output video, max is 14.", show_default=True, default=4)
+def main(output_directory: str, split: int, flip: int, fps: int) -> bool:
+    if capture(output_dir=output_directory, split_s=split, flip_mode=flip, fps=fps):
         exit(os.EX_OK)
     else:
         exit(os.EX_IOERR)
--- capture/ratcam_capture/capture.py
+++ capture/ratcam_capture/capture.py
@@ -7,37 +7,30 @@ gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GObject
 
 def bus_call(bus, message, loop):
-    t = message.type
-    if t == Gst.MessageType.EOS:
-        sys.stdout.write("End-of-stream\n")
+    if message.type == Gst.MessageType.EOS:
+        print("End of stream")
         loop.quit()
-    elif t == Gst.MessageType.ERROR:
-        err, debug = message.parse_error()
-        sys.stderr.write("Error: %s: %s\n" % (err, debug))
+    elif message.type == Gst.MessageType.ERROR:
+        error, debug = message.parse_error()
+        print(f"Error: {error}: {debug}", file=sys.stderr)
         loop.quit()
     return True
 
 
-def capture(output_dir: str) -> None:
+def capture(output_dir: str, split_s: int, flip_mode: int, fps: int) -> None:
   GObject.threads_init()
   Gst.init(None)
 
-  fps = 2
   output_dir = os.path.join(output_dir, datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
-  flip_method = 2 # 180 deg
-  timesplit_24h = 8.64e+13
-  timesplit_1h = 3.6e+12
-  timesplit_10min = 6e+11
-  timesplit_10s = 1e+10
-
   os.makedirs(output_dir, exist_ok=True)
 
   pipeline_str = f"\
     nvarguscamerasrc sensor_id=0 ! video/x-raw(memory:NVMM),width=4608,height=2592,framerate=14/1, format=NV12 ! \
-    nvvidconv flip-method={flip_method} ! video/x-raw,width=2304,height=1296 ! \
+    nvvidconv flip-method={flip_mode} ! video/x-raw,width=2304,height=1296 ! \
     videorate ! video/x-raw,width=2304,height=1296,framerate={fps}/1 ! \
     clockoverlay halignment=right valignment=bottom shaded-background=true time-format='%D %H:%M:%S' ! \
-    x264enc ! splitmuxsink location={output_dir}/video%02d.mp4 max-size-time={int(timesplit_10s)}"
+    x264enc ! \
+    splitmuxsink location={output_dir}/video%04d.mp4 max-size-time={int(split_s*1e+9)} async-handling=1 send-keyframe-requests=1"
 
   pipeline = Gst.parse_launch(pipeline_str)
   bus = pipeline.get_bus()
